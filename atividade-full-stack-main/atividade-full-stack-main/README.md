1ce9683998d017bc73ea57b1559e40e6efbd5479
rafaelalvesagra <95288805+rafaelalvesagra@users.noreply.github.com>
2023-05-09 11:11:17 -0300

    Add files via upload

---
+++ atividade-full-stack-main/atividade-full-stack-main/README.md
@@ -0,0 +1,6 @@
+# atividade-full-stack desenvolvida as pressas faltando 48h e faltando o back end ainda... (estou ficando louco)
+para as pessoas que estao lendo... saiba que eu preciso de um emprego
+por que voce esta aqui ainda:;
+(nao tem pontuaçao pq o teclado esta em outra lingua e n sei ajeitar) sou burro@ (finja que é uma interrogaçao.)
+entao... é isso galera, saiba q estou bem e alegre pois hoje comi galeto de graça. 
+A VIDA É BELA.
---
+++ atividade-full-stack-main/atividade-full-stack-main/atividade fullstack/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Ativação do Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Intercepta as requisições de rede
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
